7309aefa370e271012473b0a9104ea66805af733
Kian Kianpisheh <kian@dgp.toronto.edu>
2023-10-03 16:06:37 -0400

    tested on color features

--- features.py
+++ features.py
@@ -2,8 +2,11 @@ import cv2
 import numpy as np
 
 
+from utils import resize_max
+
+
 def get_colors(frame):
-    features = {}
+    color_data = {}
     hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     color_thresh = {
         "red": [[0, 100, 20], [10, 255, 255]],
@@ -28,12 +31,84 @@ def get_colors(frame):
                 np.array([180, 255, 255]),
             )
 
-        features[color] = cv2.bitwise_and(frame, frame, mask=feat_mask)
+        color_data[color] = cv2.bitwise_and(frame, frame, mask=feat_mask)
 
-        # creating a black border border for the frame
-        black = [128, 128, 128]
-        features[color] = cv2.copyMakeBorder(
-            features[color], 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=black
-        )
+        # # creating a black border border for the frame
+        # black = [128, 128, 128]
+        # color_data[color] = cv2.copyMakeBorder(
+        #     color_data[color], 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=black
+        # )
 
-    return features
+    return color_data
+
+
+def get_edges(frame):
+    # edge features
+    # Apply Canny edge detection
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray_frame, 100, 200, apertureSize=3)
+    gradient_x = cv2.Sobel(edges, cv2.CV_64F, 1, 0, ksize=3)
+    gradient_y = cv2.Sobel(edges, cv2.CV_64F, 0, 1, ksize=3)
+    gradient_magnitude = np.sqrt(gradient_x**2 + gradient_y**2)
+    gradient_direction = np.arctan2(gradient_y, gradient_x) * 180 / np.pi
+    # Define angle threshold for diagonal edges
+    angle_threshold = (35, 55)
+
+    # Apply the Hough Line Transform
+    lines = cv2.HoughLines(
+        edges, 1.5, np.pi / 180, threshold=80
+    )  # You can adjust the threshold
+
+    # Draw the detected lines on the original image
+    if lines is not None:
+        for rho, theta in lines[:, 0]:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * (a))
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * (a))
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)  # Draw the line in red
+
+    # Create a mask for diagonal edges
+    diagonal_edge_mask = (
+        (gradient_direction >= angle_threshold[0])
+        & (gradient_direction <= angle_threshold[1])
+    ).astype(np.uint8) * 255
+
+    kernel = np.ones((3, 3), np.uint8)
+
+    dilated_image = cv2.dilate(diagonal_edge_mask, kernel, iterations=1)
+    diagonal_edge_mask = cv2.bitwise_and(dilated_image, edges)
+    # black = [128, 128, 128]
+    # edges = cv2.copyMakeBorder(edges, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=black)
+    # diagonal_edge_mask = cv2.copyMakeBorder(
+    #     diagonal_edge_mask, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=black
+    # )
+    edges = cv2.merge((edges, edges, edges))
+    diagonal_edge_mask = cv2.merge(
+        (diagonal_edge_mask, diagonal_edge_mask, diagonal_edge_mask)
+    )
+
+    return {"edges": edges, "45": diagonal_edge_mask}
+
+
+def get_color_features(colors_data):
+    color_features = {}
+    for color in colors_data:
+        nonzeros = [np.count_nonzero(colors_data[color][:, :, i]) for i in range(3)]
+        color_features[color] = (3 * max(nonzeros)) / colors_data[color].size
+
+    return color_features
+
+
+def get_features(frame):
+    # get colors
+    colors_data = get_colors(frame)
+
+    color_features = get_color_features(colors_data)
+    x = [v for v in color_features.values()]
+
+    return x
--- main.py
+++ main.py
@@ -1,90 +1,78 @@
 import os
+import re
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
 
 import cv2
-import numpy as np
 
 from utils import resize_max
-from features import get_colors
+from features import *
 
 cap = cv2.VideoCapture("http://10.0.0.200:4747/video")
 
 
-image_seq_path = "./images"
+image_seq_path = "./images/training"
+test_image_seq_path = "./images/test"
 source = "image-seq"
+
+# training
+X = []
+Y = []
 images_files = os.listdir(image_seq_path)
+for image_file in images_files:
+    frame = cv2.imread(image_seq_path + "/" + image_file)
+    frame = resize_max(frame, 500)
+    x = get_features(frame)
+    y = parts = re.split(r"\d+", image_file)[0]
+
+    X.append(x)
+    Y.append(y)
+
+# training
+model = DecisionTreeClassifier()
+model.fit(X, Y)
+
+model2 = LogisticRegression()
+model2.fit(X, Y)
 
 i = 0
+features = []
+images_files = os.listdir(test_image_seq_path)
 while True:
     if i == len(images_files):
         break
-    if source == "video":
+    if source == "phone-camera":
         ret, frame = cap.read(0)
         frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
         frame = cv2.resize(
             frame, ((int(frame.shape[1] * 0.7)), (int(frame.shape[0] * 0.7)))
         )
     elif source == "image-seq":
-        frame = cv2.imread(image_seq_path + "/" + images_files[i])
+        frame = cv2.imread(test_image_seq_path + "/" + images_files[i])
         frame = resize_max(frame, 500)
         i += 1
 
+    x = get_features(frame)
+    print("decision tree: ", model.predict([x])[0])
+    print("logistic regression: ", model2.predict([x])[0])
+
     frame_filtered = cv2.GaussianBlur(frame, (11, 11), 0)
-    features = get_colors(frame_filtered)
+    colors_data = get_colors(frame_filtered)
 
-    # edge features
-    # Apply Canny edge detection
-    gray_frame = cv2.cvtColor(frame_filtered, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(gray_frame, 100, 200, apertureSize=3)
-    gradient_x = cv2.Sobel(edges, cv2.CV_64F, 1, 0, ksize=3)
-    gradient_y = cv2.Sobel(edges, cv2.CV_64F, 0, 1, ksize=3)
-    gradient_magnitude = np.sqrt(gradient_x**2 + gradient_y**2)
-    gradient_direction = np.arctan2(gradient_y, gradient_x) * 180 / np.pi
-    # Define angle threshold for diagonal edges
-    angle_threshold = (35, 55)
+    # feature extraction
+    color_features = get_color_features(colors_data)
+    edge_features = get_edges(frame_filtered)
 
-    # Apply the Hough Line Transform
-    lines = cv2.HoughLines(
-        edges, 1.5, np.pi / 180, threshold=80
-    )  # You can adjust the threshold
-
-    # Draw the detected lines on the original image
-    if lines is not None:
-        for rho, theta in lines[:, 0]:
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            x1 = int(x0 + 1000 * (-b))
-            y1 = int(y0 + 1000 * (a))
-            x2 = int(x0 - 1000 * (-b))
-            y2 = int(y0 - 1000 * (a))
-            cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)  # Draw the line in red
-
-    # Create a mask for diagonal edges
-    diagonal_edge_mask = (
-        (gradient_direction >= angle_threshold[0])
-        & (gradient_direction <= angle_threshold[1])
-    ).astype(np.uint8) * 255
-
-    kernel = np.ones((3, 3), np.uint8)
-
-    dilated_image = cv2.dilate(diagonal_edge_mask, kernel, iterations=1)
-    diagonal_edge_mask = cv2.bitwise_and(dilated_image, edges)
-    black = [128, 128, 128]
-    edges = cv2.copyMakeBorder(edges, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=black)
-    diagonal_edge_mask = cv2.copyMakeBorder(
-        diagonal_edge_mask, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=black
-    )
-    edges = cv2.merge((edges, edges, edges))
-    diagonal_edge_mask = cv2.merge(
-        (diagonal_edge_mask, diagonal_edge_mask, diagonal_edge_mask)
-    )
-
-    frame = cv2.copyMakeBorder(frame, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=black)
+    # frame = cv2.copyMakeBorder(frame, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=[128, 128, 128])
 
     # display frames and the extracted features
-    row1 = cv2.hconcat((frame, features["red"], features["green"], features["blue"]))
-    row2 = cv2.hconcat((features["yellow"], edges, diagonal_edge_mask, frame))
+    row1 = cv2.hconcat(
+        (frame, colors_data["red"], colors_data["green"], colors_data["blue"])
+    )
+    row2 = cv2.hconcat(
+        (colors_data["yellow"], edge_features["edges"], edge_features["45"], frame)
+    )
     all_iimages = cv2.vconcat((row1, row2))
     cv2.imshow("Original Image", all_iimages)
 
